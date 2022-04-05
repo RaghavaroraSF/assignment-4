a43e72150305b47ccfcbd5888b02f94cd7d3853c
RaghavaroraSF <97958393+RaghavaroraSF@users.noreply.github.com>
2022-04-05 16:05:42 +0530

    commit

---
+++ dist/User.js
@@ -0,0 +1,11 @@
+export class User {
+    constructor(firstName, middleName, lastName, email, phone_no, role, address) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone_no = phone_no;
+        this.role = role;
+        this.address = address;
+    }
+}
